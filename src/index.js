260b9e7a24a3d3ce8420538e48625018cd131f1e
badiaz <badiaz@uninorte.edu.co>
2021-11-25 20:13:00 -0500

    versión 25 de nov

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
